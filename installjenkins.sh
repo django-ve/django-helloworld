ad3ece9f57a055dfaba3d8baca39eb7a751cabf9
EC2 Default User <ec2-user@ip-172-31-87-163.ec2.internal>
2024-05-25 11:58:48 +0000

    added to git

---
+++ installjenkins.sh
@@ -0,0 +1,9 @@
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo dnf install java-17-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
