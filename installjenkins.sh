ad4c2a0b63beb89c5c95344c5e990ee0843770bb
EC2 Default User <ec2-user@ip-172-31-35-53.ap-south-1.compute.internal>
2024-05-30 06:59:56 +0000

    added support for installing java other way as well

--- installjenkins.sh
+++ installjenkins.sh
@@ -4,6 +4,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 sudo dnf install java-17-amazon-corretto -y
+sudo yum install java-17-amazon-corretto -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
